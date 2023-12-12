a2cde2213ded5375117ac70bc445bbe62767e190
diegoreckziegel <52861095+diegoreckziegel@users.noreply.github.com>
2023-12-12 21:25:33 +0000

    add `database.js` and `.env` files

---
+++ infra/database.js
@@ -0,0 +1,15 @@
+import { Client } from "pg";
+
+async function query(SQL) {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  });
+
+  await client.connect();
+  const result = await client.query(SQL);
+  console.log(result.rows);
+}
+
+export default { query: query };
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  await database.query("SELECT 1+1 soma;");
   response.status(200).json("status é legal");
 }
 
